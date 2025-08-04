90954c6f634c35e77739a3a8041e7586273435ab
Atieh Rahmani <erassis73@gmail.com>
2025-08-05 01:35:28 +0330

    Add files via upload

---
+++ optimistic/bandit.py
@@ -0,0 +1,26 @@
+
+
+
+import numpy as np
+
+class Bandit:
+    def __init__(self, q, Q):
+        self.q = q
+        self.Q = Q
+        self.n = 0
+
+    def __str__(self):
+        return f"q:{self.q} | Q:{self.Q} | n:{self.n}"
+
+    def pull(self):
+        return np.random.normal(self.q, 1)
+
+    def update(self, r):
+        self.n += 1
+        self.Q = self.Q + (1/self.n) * (r - self.Q)
+
+def simple_bandit(q):
+    return Bandit(q=q,Q=0)
+
+def optimistic_bandit(q,Q):
+    return Bandit(q=q,Q=Q)
